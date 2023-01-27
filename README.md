027a0835e01fc413319f3cf7c510c5bce7415acd
colabogey <colabogey@protonmail.com>
2023-01-27 10:08:08 -0500

    improved README; delete dead code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # webasm
 ## research into using webassembly
+
+Basically,
+- a C++ class that does basic math
+- the class lives in an archive library
+- export functions access the class members
+  - simply creates a unique_ptr and accesses the object
+  - these use the KEEP_ALIVE tag
+- build creates client side and server side js
+  - server side uses MODULARIZE
+- html files use client side js and uses
+  - ccall
+  - cwrap
+  - the so called streaming method that uses the wasm file directly
+- server side includes additions
+  - to call ccall
+  - runnable via node.js
--- basicMathControl/src/server_side/js/node_additions.js
+++ basicMathControl/src/server_side/js/node_additions.js
@@ -7,21 +7,18 @@ No clue is this is the way its supposed to be done.
 
 */
 
-var factory = require('./basicMathControl.js');
+let factory = require('./basicMathControl.js');
 
 factory().then((instance) => {
-  var arguments_ = [];
-  var method = "";
-  var arg1 = "0";
-  var arg2 = "0";
+  let arguments_ = [];
+  let method = "";
+  let arg1 = "0";
+  let arg2 = "0";
   arguments_ = process['argv'].slice(2);
   method = arguments_[0];
   arg1 = arguments_[1];
   arg2 = arguments_[2];
-  //instance._basicMathAdd(5, 6); // direct calling works
-  //var x = instance.ccall("basicMathAdd", "number", ["number", "number"], [4, 2]); // using ccall etc. also work
-  var y = instance.ccall(method, "number", ["number", "number"], 
+  let y = instance.ccall(method, "number", ["number", "number"], 
                           [arg1, arg2]); // using ccall etc. also work
   console.log(`${method} ${arg1} ${arg2}: ${y}`);
-  //console.log(instance._basicMathAdd(7, 8)); // values can be returned, etc.
 });
